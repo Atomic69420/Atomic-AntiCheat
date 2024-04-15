68cbd7b06a2ac72d6d5444072e963ae3b87446b0
Atomic69420 <116735034+Atomic69420@users.noreply.github.com>
2024-04-15 21:39:08 +0200

    Seed hider
    set to false by default due to issues

---
+++ modules/seedhide.ts
@@ -0,0 +1,51 @@
+import * as fs from "fs";
+import * as path from "path";
+import { pdb, pdatar, sendwebhook, embed } from "../index";
+import { events } from "bdsx/event";
+import { MinecraftPacketIds } from "bdsx/bds/packetids";
+import { bedrockServer } from "bdsx/launcher";
+import { CANCEL } from "bdsx/common";
+import { serverProperties } from "bdsx/serverproperties";
+    console.log('[Atomic-AntiCheat] Loaded Seed Hider')
+    interface acconfig {
+      discord: string;
+      prefix: string;
+      webhook: string;
+      modules: {
+        bot: {
+          [key: string]: boolean;
+          T1?: boolean;
+          T2?: boolean;
+          T3?: boolean;
+          T4?: boolean;
+        };
+        badpacket: {
+          [key: string]: boolean;
+          T1?: boolean;
+        }
+        speed: {
+          [key: string]: boolean;
+          T1?: boolean;
+        },
+        crasher: {
+          [key: string]: boolean;
+          T1?: boolean;
+          T2?: boolean;
+        },
+        reach: {
+            [key: string]: boolean;
+            T1?: boolean;
+          },
+          seedhider: boolean;
+      };
+    }
+    const configdata = fs.readFileSync(path.join(__dirname, "../config.json"), 'utf8');
+    const config: acconfig = JSON.parse(configdata);
+    if (config.modules.seedhider === true) {
+        if (serverProperties["client-side-chunk-generation-enabled"] === true) {
+            console.warn(`Seed hider causes issues when client-side-chunk-generation-enabled is set to true in server.properties please disable it or set seedhider to false in config.json for no further issues`)
+        }
+        events.packetSend(MinecraftPacketIds.StartGame).on((pkt, ni) => {
+            pkt.settings.seed = 0;
+          });
+    }
