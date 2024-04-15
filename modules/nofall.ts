5447db36a69d09c5b3bad8bc571602be6c8469c8
Atomic69420 <116735034+Atomic69420@users.noreply.github.com>
2024-04-15 21:23:40 +0200

    nofall T1

---
+++ modules/nofall.ts
@@ -0,0 +1,99 @@
+import * as fs from "fs";
+import * as path from "path";
+import { pdb, pdatar, sendwebhook, embed } from "../index";
+import { events } from "bdsx/event";
+import { MinecraftPacketIds } from "bdsx/bds/packetids";
+import { bedrockServer } from "bdsx/launcher";
+import { CANCEL } from "bdsx/common";
+
+    console.log('[Atomic-AntiCheat] Loaded NoFall Detections')
+    interface acconfig {
+      discord: string;
+      prefix: string;
+      webhook: string;
+      modules: {
+        bot: {
+          [key: string]: boolean;
+          T1?: boolean;
+          T2?: boolean;
+          T3?: boolean;
+          T4?: boolean;
+        };
+        badpacket: {
+          [key: string]: boolean;
+          T1?: boolean;
+        }
+        speed: {
+          [key: string]: boolean;
+          T1?: boolean;
+        },
+        crasher: {
+          [key: string]: boolean;
+          T1?: boolean;
+          T2?: boolean;
+        },
+        reach: {
+            [key: string]: boolean;
+            T1?: boolean;
+          },
+          badskin: {
+            [key: string]: boolean;
+            T1?: boolean;
+            T2?: boolean;
+          },
+          nofall: {
+            [key: string]: boolean;
+            T1?: boolean;
+          }
+      };
+    }
+    let rps: number = 0
+    setInterval(() => {
+        rps = 0
+    }, 1000);
+    const configdata = fs.readFileSync(path.join(__dirname, "../config.json"), 'utf8');
+    const config: acconfig = JSON.parse(configdata);
+    events.packetBefore(MinecraftPacketIds.PlayerAction).on((pkt, ni) => {
+        if (config.modules.nofall.T1 === false) return;
+        const pdata: pdatar | undefined = pdb.get(ni.toString().split(":")[0]);
+        if (pkt.action === 7) {
+        rps++
+        }
+        if (rps >= 3) {
+            if (pdata) {
+                bedrockServer.serverInstance.disconnectClient(ni, `${config.prefix}\nYou Have Been Kicked!\nReason: NoFall [T1]\nDiscord: ${config.discord}`);
+                    console.log(`${config.prefix}\nPlayer ${pdata.username} was kicked for NoFall [T1] This means the player sent more than 2 respawn actions within a second.`);
+                    
+                    if (config.webhook !== "None") {
+                        const embeds: embed[] = [
+                            {
+                                title: 'NoFall [T1]',
+                                description: `Kicked ${pdata.username} for NoFall [T1] This means the player sent more than 2 respawn actions within a second.`,
+                                color: 65280,
+                            },
+                        ];
+                        
+                        sendwebhook(config.webhook, embeds);
+                    }
+                    
+                    return CANCEL;
+            } else {
+                bedrockServer.serverInstance.disconnectClient(ni, `${config.prefix}\nYou Have Been Kicked!\nReason: NoFall [T1]\nDiscord: ${config.discord}`);
+                console.log(`${config.prefix}\nA player was kicked for NoFall [T1] This means the player sent more than 2 respawn actions within a second.`);
+                
+                if (config.webhook !== "None") {
+                    const embeds: embed[] = [
+                        {
+                            title: 'NoFall [T1]',
+                            description: `Kicked a player for NoFall [T1] This means the player sent more than 2 respawn actions within a second.`,
+                            color: 65280,
+                        },
+                    ];
+                    
+                    sendwebhook(config.webhook, embeds);
+                }
+                
+                return CANCEL;
+            }
+        }
+    })
